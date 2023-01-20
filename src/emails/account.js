6d88d749b348c60fa31f8ceb11fa34673cd0207c
Aryan Sharma <arshar1204@gmail.com>
2023-01-21 01:25:24 +0800

    Email verification completed

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,18 +5,19 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'arshar1204@gmail.com',
+        from: process.env.SENDER_ADDRESS,
         subject: 'Thanks for joining in!',
-        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+        text: `Welcome to the app, ${name}. Please try to keep your task quotas per month below 100, as I can only afford so much
+        database charges, thanks for understanding.`
     })
 }
 
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'arshr1204@gmail.com',
+        from: process.env.SENDER_ADDRESS,
         subject: 'Sorry to see you go!',
-        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+        text: `Goodbye, ${name}. I will be deleting all your old tasks from the database`
     })
 }
 
