49bf7dfba1b82788293c3b28db2a98b5761a74cd
Aryan Sharma <arshar1204@gmail.com>
2023-01-21 00:19:15 +0800

    Fixed user authentication

--- src/models/user.js
+++ src/models/user.js
@@ -60,7 +60,7 @@ userSchema.methods.generateAuthToken = async function () {
     const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({ token })
     await user.save()
-
+    return token
 }
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email: email })
